09a85c67522c586583160c92ac1fa06eb424e27a
Invincible-21 <shantobd1969@gmail.com>
2023-02-26 20:06:34 +0600

    updated

---
+++ Recursion/helloRecursive.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void hello(){
+    cout<<"Hello World!\n";
+    hello();
+}
+
+int main(){
+
+    hello();
+
+    return 0;
+}
+
+
+//press ' ctrl + c ' to terminate :-D
